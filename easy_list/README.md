25b5b5ef1b4fab4b113fc9dcbc796bccda7457a4
Imran <imran@zeropoint.it>
2019-01-03 14:58:34 +0500

    Printing the event methods in the logs now which were called.

--- easy_list/README.md
+++ easy_list/README.md
@@ -14,3 +14,23 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our 
 [online documentation](https://flutter.io/docs), which offers tutorials, 
 samples, guidance on mobile development, and a full API reference.
+
+
+### Running Lifecycle
+
+- Running for the first time
+`
+I/flutter (14370): [ProductManager Widget] Constructor
+I/flutter (14370): [ProductManager Widget] createState()
+I/flutter (14370): [ProductManager State] initState()
+I/flutter (14370): [ProductManager State] build()
+I/flutter (14370): [Products Widget] Constructor
+I/flutter (14370): [Products Widget] build()
+`
+
+- Clicking on Add Product generates the following cycle
+`
+I/flutter (14370): [ProductManager State] build()
+I/flutter (14370): [Products Widget] Constructor
+I/flutter (14370): [Products Widget] build()
+`
--- easy_list/lib/product_manager.dart
+++ easy_list/lib/product_manager.dart
@@ -5,10 +5,13 @@ import './products.dart';
 class ProductManager extends StatefulWidget {
   final String startingProduct;//will be changed from outside
 
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     // TODO: implement createState
     return _ProductManagerState();
   }
@@ -19,12 +22,20 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override
   void initState() {//runs before build runs
+    print('[ProductManager State] initState()');
     _products.add(widget.startingProduct);
     super.initState();
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(
       children: [
         Container(

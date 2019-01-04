8dfbc62e7a64c78919ff7061bad523ff7437d05e
Imran <imran@zeropoint.it>
2019-01-04 20:04:59 +0500

    Added confirmation dialog on deleting product.

--- easy_list/lib/pages/product.dart
+++ easy_list/lib/pages/product.dart
@@ -7,6 +7,33 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
+  _showWarningDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('DISCARD'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('CONTINUE'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              )
+            ],
+          );
+        });
+    //Navigator.pop(context, true)
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -29,7 +56,7 @@ class ProductPage extends StatelessWidget {
                   child: RaisedButton(
                     color: Theme.of(context).accentColor,
                     child: Text('DELETE'),
-                    onPressed: () => Navigator.pop(context, true),
+                    onPressed: () => _showWarningDialog(context),
                   ))
             ],
           ),

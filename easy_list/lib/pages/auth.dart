8bca35959ffada47172b7756e86548fd33058699
Imran <imran@zeropoint.it>
2019-01-23 16:52:12 +0500

    Implemented login feature using Firebase.

--- easy_list/lib/pages/auth.dart
+++ easy_list/lib/pages/auth.dart
@@ -25,7 +25,7 @@ class AuthPageState extends State<AuthPage> {
     return DecorationImage(
         fit: BoxFit.cover,
         colorFilter:
-        ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
+            ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
         image: AssetImage('assets/background.jpg'));
   }
 
@@ -33,7 +33,7 @@ class AuthPageState extends State<AuthPage> {
     return TextFormField(
       validator: (String value) {
         if (!RegExp(
-            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
             .hasMatch(value)) {
           return 'Enter a valid email address e.g. example@example.com';
         }
@@ -84,33 +84,41 @@ class AuthPageState extends State<AuthPage> {
 
     formKey.currentState.save();
 
+    Map resInfo;
+    model.toggleIsLoading();
     if (_authMode == AuthMode.Login) {
-      model.login(formData['email'], formData['password']);
+      resInfo = await model.login(formData['email'], formData['password']);
     } else {
-      final Map resInfo = await model.signup(
-          formData['email'], formData['password']);
-      if (resInfo['success']) {
-        Navigator.pushReplacementNamed(context, '/home');
-      } else {
-        showDialog(context: context, builder: (BuildContext contenxt) {
-          return AlertDialog(title: Text('An error occured!'),
-            content: Text(resInfo['message']),
-            actions: <Widget>[
-              FlatButton(child: Text('OK'), onPressed: () {
-                Navigator.of(context).pop();
-              },)
-            ],);
-        });
-      }
+      final Map resInfo =
+          await model.signup(formData['email'], formData['password']);
+    }
+    model.toggleIsLoading();
+
+    if (resInfo['success']) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext contenxt) {
+            return AlertDialog(
+              title: Text('An error occured!'),
+              content: Text(resInfo['message']),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final double deviceWidth = MediaQuery
-        .of(context)
-        .size
-        .width; //Media query
+    final double deviceWidth = MediaQuery.of(context).size.width; //Media query
     final double targetWidth = deviceWidth > 768.0 ? 500.0 : deviceWidth * 0.95;
 
     return Scaffold(
@@ -123,55 +131,56 @@ class AuthPageState extends State<AuthPage> {
           child: Center(
               child: SingleChildScrollView(
                   child: Container(
-                    width: targetWidth, //80% of our device width
-                    child: Form(
-                        key: formKey,
-                        child: Column(children: <Widget>[
-                          _buildEmailTextField(),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          _buildPasswordTextField(),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          _authMode == AuthMode.Signup
-                              ? _buildPasswordConfirmTextField()
-                              : Container(),
-                          _authMode == AuthMode.Signup
-                              ? SizedBox(
-                            height: 10.0,
-                          )
-                              : Container(),
-                          FlatButton(
-                            child: Text(
-                                'Switch to ${_authMode == AuthMode.Login
-                                    ? 'Signup'
-                                    : 'Login'}'),
-                            onPressed: () {
-                              setState(() {
-                                _authMode = _authMode == AuthMode.Login
-                                    ? AuthMode.Signup
-                                    : AuthMode.Login;
-                              });
-                            },
-                          ),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          ScopedModelDescendant<MainModel>(
-                            builder:
-                                (BuildContext context, Widget child,
-                                MainModel model) {
-                              return RaisedButton(
-                                textColor: Colors.white,
-                                onPressed: () => _submitForm(model),
-                                child: Text('LOGIN'),
-                              );
-                            },
-                          ),
-                        ])),
-                  ))),
+            width: targetWidth, //80% of our device width
+            child: Form(
+                key: formKey,
+                child: Column(children: <Widget>[
+                  _buildEmailTextField(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  _buildPasswordTextField(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  _authMode == AuthMode.Signup
+                      ? _buildPasswordConfirmTextField()
+                      : Container(),
+                  _authMode == AuthMode.Signup
+                      ? SizedBox(
+                          height: 10.0,
+                        )
+                      : Container(),
+                  FlatButton(
+                    child: Text(
+                        'Switch to ${_authMode == AuthMode.Login ? 'Signup' : 'Login'}'),
+                    onPressed: () {
+                      setState(() {
+                        _authMode = _authMode == AuthMode.Login
+                            ? AuthMode.Signup
+                            : AuthMode.Login;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ScopedModelDescendant<MainModel>(
+                    builder:
+                        (BuildContext context, Widget child, MainModel model) {
+                      return model.isLoading
+                          ? CircularProgressIndicator()
+                          : RaisedButton(
+                              textColor: Colors.white,
+                              onPressed: () => _submitForm(model),
+                              child: Text(_authMode == AuthMode.Login
+                                  ? 'LOGIN'
+                                  : 'SIGNUP'),
+                            );
+                    },
+                  ),
+                ])),
+          ))),
         ));
   }
 }
--- easy_list/lib/scoped_models/user.dart
+++ easy_list/lib/scoped_models/user.dart
@@ -6,25 +6,46 @@ import '../models/user.dart';
 
 mixin UserModel on Model {
   User _authUser;
+  final authEndpoint =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/';
+  final authKey = 'AIzaSyBxr8j7o3760Nvp-kkDgJK7IcAsUQBCbTs';
 
   User get authUser {
     return _authUser;
   }
 
-  void login(String email, String password) {
-    _authUser = User(id: '1', email: email, password: password);
+  Future<Map> login(String email, String password) async {
+    Map authData = this.authPayload(email, password);
+
+    final http.Response res = await http.post(
+        this.authEndpoint + 'verifyPassword?key=$authKey',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'});
+    //authUser = User(id: '1', email: email, password: password);
+
+    final Map resBody = json.decode(res.body);
+
+    bool hasError = false;
+    String message = 'Auth successeeded';
+    if (resBody.containsKey('error')) {
+      hasError = true;
+      if (resBody['error']['message'] == 'EMAIL_NOT_FOUND' ||
+          resBody['error']['message'] == 'INVALID_PASSWORD') {
+        message = 'Wrong email or password.';
+      } else {
+        message = 'Something went wrong.';
+      }
+    }
+
+    return {'success': !hasError, 'message': message};
   }
 
   Future<Map> signup(String email, String password) async {
-    final Map authData = {
-      //required by google auth
-      'email': email,
-      'password': password,
-      'returnSecureToken': true
-    };
+    Map authData = this.authPayload(email, password);
 
     final http.Response res = await http.post(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBxr8j7o3760Nvp-kkDgJK7IcAsUQBCbTs',
+        this.authEndpoint +
+            'signupNewUser?key=AIzaSyBxr8j7o3760Nvp-kkDgJK7IcAsUQBCbTs',
         body: json.encode(authData),
         headers: {'Content-Type': 'application/json'});
 
@@ -32,9 +53,9 @@ mixin UserModel on Model {
 
     bool hasError = false;
     String message = 'Auth successeeded';
-    if(resBody.containsKey('error')) {
+    if (resBody.containsKey('error')) {
       hasError = true;
-      if(resBody['error']['message'] == 'EMAIL_EXISTS') {
+      if (resBody['error']['message'] == 'EMAIL_EXISTS') {
         message = 'Email already exists.';
       } else {
         message = 'Something went wrong.';
@@ -42,5 +63,14 @@ mixin UserModel on Model {
     }
 
     return {'success': !hasError, 'message': message};
+  }
+
+  Map authPayload(String email, String password) {
+    return {
+      //required by google auth
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
   }
 }
